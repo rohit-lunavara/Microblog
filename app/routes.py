b72e39079f25247aad1d10540514e0c272da243b
rohit-lunavara <rohit.lunavara@gmail.com>
2020-07-26 03:20:35 -0400

    Part 14 - AJAX complete

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -7,6 +7,9 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 from app.email import send_password_reset_email
 from flask_babel import _, get_locale
+from guess_language import guess_language
+import json
+import requests
 
 @app.route('/', methods = ["GET", "POST"])
 @app.route('/index', methods = ["GET", "POST"])
@@ -14,7 +17,10 @@ from flask_babel import _, get_locale
 def index() :
     form = PostForm()
     if form.validate_on_submit() :
-        post = Post(body = form.post.data, author = current_user)
+        language = guess_language(form.post.data)
+        if language == "UNKNOWN" or len(language) > 5 :
+            language = ""
+        post = Post(body = form.post.data, author = current_user, language = language)
         db.session.add(post)
         db.session.commit()
         flash(_("Your post is now live!"))
@@ -166,3 +172,21 @@ def reset_password_request() :
         return redirect(url_for('login'))
     return render_template("reset_password_request.html", title = _("Reset Password"), form = form)
 
+def translate(text, source_language, dest_language):
+    if 'MS_TRANSLATOR_KEY' not in app.config or \
+            not app.config['MS_TRANSLATOR_KEY']:
+        return _('Error: the translation service is not configured.')
+    auth = {'Ocp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY']}
+    r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc/Translate?text={}&from={}&to={}'.format(
+        text, source_language, dest_language), headers=auth
+    )
+    if r.status_code != 200:
+        return _('Error: the translation service failed.')
+    return json.loads(r.content.decode('utf-8-sig'))
+
+@app.route('/translate', methods = ["POST"])
+@login_required
+def translate_text() :
+    return jsonify({
+        "text" : translate(request.form["text"], request.form("source_language"), request.form["dest_language"])
+    })
